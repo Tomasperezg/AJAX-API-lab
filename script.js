552ea9f65dc5f4acb474eb9a43c6322ac2759112
tomas <perezgantomas@gmail.com>
2018-05-14 22:18:32 -0400

    tittle added

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ $.get("https://www.reddit.com/r/aww/.json").then(function (json) {
 
     let text = json.data.children;
 	   let target = $("#title1");
-      // console.log(json.data.children);
-      // document.getElementById('title1').innerHTML = (json.data.children[0].data.title);
+
       for(i=0;i<10;i++){
         var titles  = text[i].data.title;
         var urls = text[i].data.url;
         target.append(`<a href = ${text[i].data.url}><p>${i}</p><img src="${text[i].data.thumbnail}"/></a>`);
         $(target.append(titles)).css({"color": "blue", "display":"block", "margin-bottom": "20px"});
-        target.append(urls);
+        $(target.append(urls)).css({"color": "black"});
       }
     });
